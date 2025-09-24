4c09b0a80d8f233453bbf5ce46e50aec8104c05a
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-09-24 23:09:19 +0700

    d

--- src/node/mods/mod.test.ts
+++ src/node/mods/mod.test.ts
@@ -1,6 +1,6 @@
 import { assert, test } from "@hazae41/phobos";
 import { Buffer } from "node:buffer";
-import { Memory, Secp256k1SigningKey, Secp256k1VerifyingKey, initBundled } from "./mod.js";
+import { Memory, Secp256k1SigningKey, Secp256k1VerifyingKey, initBundled } from "./mod.ts";
 
 function equals(a: Uint8Array, b: Uint8Array) {
   const ba = Buffer.from(a)
