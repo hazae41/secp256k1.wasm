a68470e8b70bd9f9466dd51c5acbb69eb2018c80
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-10-27 21:56:45 +0700

    f

--- src/node/mods/mod.ts
+++ src/node/mods/mod.ts
@@ -1,9 +1,15 @@
 export * from "../../wasm/pkg/secp256k1_wasm.js";
 
 /* @ts-types="../../wasm/pkg/secp256k1_wasm.d.ts" */
-import init, { type InitOutput } from "../../wasm/pkg/secp256k1_wasm.js";
+import init, {
+  initSync, type InitOutput
+} from "../../wasm/pkg/secp256k1_wasm.js";
 import { data } from "../../wasm/pkg/secp256k1_wasm.wasm.js";
 
-export async function initBundled(): Promise<InitOutput> {
+export async function load(): Promise<InitOutput> {
   return await init({ module_or_path: data })
+}
+
+export function loadSync(): InitOutput {
+  return initSync({ module_or_path: data });
 }
